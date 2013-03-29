c3a3eed02a954723284c665277faa8301580581a
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2013-03-28 19:07:05 -0700

    uvm updates

--- .bashrc.d/70-uvm.bash
+++ .bashrc.d/70-uvm.bash
@@ -1,10 +1,10 @@
 
-
 export UVM_FOLDER=~/work/uvm
 
 if [ -f $UVM_FOLDER/setup.sh ] ; then
   . $UVM_FOLDER/setup.sh
 fi
 
+export UVM_BUILD_SHARED_ONLY=
+export UVM_ROS=
 export UVM_NO_COLORGCC=
-export UVM_MUELLEIMER=~/work/UVM/uvm_muelleimer/ubuntu_gcc_463_12.04__64bit_x64
